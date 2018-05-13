b2f7d6fa6ea0cf723fbacdc5621239f365fea814
andy <anyi_zhidu@126.com>
2018-05-13 22:38:52 +0800

    完成了商品添加页面的开发

--- src/main/java/com/lecotec/mixi/controller/MerchantController.java
+++ src/main/java/com/lecotec/mixi/controller/MerchantController.java
@@ -1,17 +1,5 @@
 package com.lecotec.mixi.controller;
 
-import static com.lecotec.mixi.common.AuthorityUtil.produceCookie;
-import static com.lecotec.mixi.common.ConstString.MERCHANT;
-import static com.lecotec.mixi.common.ConstString.MERCHANT_PHONE_NUMBER;
-import static com.lecotec.mixi.common.ConstString.MERCHANT_TOKEN;
-import static com.lecotec.mixi.common.ConstString.MERCHANT_USER_TYPE;
-
-import java.security.Key;
-
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import javax.validation.Valid;
-
 import com.lecotec.mixi.common.ConstString;
 import com.lecotec.mixi.common.EncryptUtil;
 import com.lecotec.mixi.common.RandomUtil;
@@ -22,24 +10,31 @@ import com.lecotec.mixi.model.response.FailResponse;
 import com.lecotec.mixi.model.response.ResponseObject;
 import com.lecotec.mixi.model.response.SuccessResponse;
 import com.lecotec.mixi.service.MerchantService;
-
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.ObjectUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+import java.security.Key;
+
+import static com.lecotec.mixi.common.AuthorityUtil.produceCookie;
+import static com.lecotec.mixi.common.ConstString.*;
 
 @RestController
 @RequestMapping("/api/merchant")
+@Api(value = "/api/merchant", tags = "商家登录、注册、忘记密码和修改密码接口集合")
 public class MerchantController {
 
     @Autowired
     private MerchantService merchantService;
 
     @PostMapping("/login/pwdLogin")
+    @ApiOperation("商家登录接口（手机号、密码），调用之前先获取RSA公钥，加密密码(先用md5转码)")
     public ResponseObject pwdLogin(@Valid @RequestBody UserParamWithPassword userParamWithPassword, HttpServletResponse response, HttpSession session) throws Exception {
         Key privateKey = (Key) session.getAttribute(ConstString.RSA_PRIVATE_KEY);
         String phoneNumber = userParamWithPassword.getPhoneNumber();
@@ -67,7 +62,7 @@ public class MerchantController {
         return new SuccessResponse(merchantService.saveMerchant(merchant));
     }
 
-    @PutMapping("/merchantPassword")
+    @PutMapping("/updateMerchantPassword")
     public ResponseObject updateMerchantPassword(@Valid @RequestBody UserParamForChangePassword userParamForChangePassword) {
         String phoneNumber = /*EncryptUtil.rsaDecrypt(*/userParamForChangePassword.getPhoneNumber()/*, privateKey)*/;
         String password = /*EncryptUtil.rsaDecrypt(*/userParamForChangePassword.getPassword()/*, privateKey)*/;
